8ffd5f9aa26a3246e3b8dd81dfe95968f6106180
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-30 14:11:58 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.SqlClient;
+
+var connStr = "Data Source = localhost; Initial Catalog = db_videogame; Integrated Security = True";
+using var conn = new SqlConnection(connStr);
+
+try
+{
+
+    conn.Open();
+
+}
+catch
+{
+
+}
+
